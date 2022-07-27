e84fb8789ebb52c464bca00a521f76fda8deda99
Joe Nace <jwnace@gmail.com>
2022-07-26 22:04:37 -0400

    added placeholders for day 11 solution

---
+++ src/aoc-2015/Day11.cs
@@ -0,0 +1,14 @@
+﻿namespace aoc_2015;
+
+public class Day11
+{
+    public static int Part1()
+    {
+        return 0;
+    }
+
+    public static int Part2()
+    {
+        return 0;
+    }
+}
---
+++ tests/aoc-2015-tests/Day11Tests.cs
@@ -0,0 +1,18 @@
+﻿using aoc_2015;
+
+namespace aoc_2015_tests;
+
+public class Day11Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(-1, Day11.Part1());
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(-1, Day11.Part2());
+    }
+}
