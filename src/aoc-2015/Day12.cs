073b1561af815d8bc51883f0eb0c963c575bab7f
Joe Nace <jwnace@gmail.com>
2022-07-27 08:27:28 -0400

    WIP day 12 solution

---
+++ src/aoc-2015/Day12.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace aoc_2015;
+
+public static class Day12
+{
+    public static int Part1()
+    {
+        var input = File.ReadAllText("../../../../../input/day12.txt");
+        var matches = Regex.Matches(input, @"[-]{0,1}\d+");
+        return matches.Sum(x => int.Parse(x.Value));
+    }
+
+    public static int Part2()
+    {
+        var input = File.ReadAllText("../../../../../input/day12.txt");
+        return 0;
+    }
+}
