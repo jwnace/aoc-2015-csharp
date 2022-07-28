981f7773aa032831dc410c6bd5676b2445b99d53
Joe Nace <jwnace@gmail.com>
2022-07-27 21:48:47 -0400

    added solution for day 13

---
+++ tests/aoc-2015-tests/Day13Tests.cs
@@ -0,0 +1,18 @@
+using aoc_2015;
+
+namespace aoc_2015_tests;
+
+public class Day13Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(733, Day13.Part1());
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(725, Day13.Part2());
+    }
+}
