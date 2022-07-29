f15c4dee5a887b6f1895c25daa0e17339f98b991
Joe Nace <jwnace@gmail.com>
2022-07-29 01:03:46 -0400

    added solution for day 16

--- src/aoc-2015/Program.cs
+++ src/aoc-2015/Program.cs
@@ -46,5 +46,8 @@ using aoc_2015;
 // Console.WriteLine($"Day 14 Part 1: {Day14.Part1()}");
 // Console.WriteLine($"Day 14 Part 2: {Day14.Part2()}");
 
-Console.WriteLine($"Day 15 Part 1: {Day15.Part1()}");
-Console.WriteLine($"Day 15 Part 2: {Day15.Part2()}");
+// Console.WriteLine($"Day 15 Part 1: {Day15.Part1()}");
+// Console.WriteLine($"Day 15 Part 2: {Day15.Part2()}");
+
+Console.WriteLine($"Day 16 Part 1: {Day16.Part1()}");
+Console.WriteLine($"Day 16 Part 2: {Day16.Part2()}");
