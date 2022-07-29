0a1ef9e9402559469cf9aede55d84b0dcb3ad29a
Joe Nace <jwnace@gmail.com>
2022-07-28 20:39:32 -0400

    WIP day 15

---
+++ src/aoc-2015/Day15.cs
@@ -0,0 +1,22 @@
+namespace aoc_2015;
+
+public static class Day15
+{
+    public static int Part1()
+    {
+        var input = File.ReadAllLines("../../../../../input/day15_sm.txt");
+
+        foreach (var line in input)
+        {
+            Console.WriteLine(line);
+        }
+        
+        return 0;
+    }
+
+    public static int Part2()
+    {
+        var input = File.ReadAllLines("../../../../../input/day15.txt");
+        return 0;
+    }
+}
---
+++ tests/aoc-2015-tests/Day15Tests.cs
@@ -0,0 +1,18 @@
+using aoc_2015;
+
+namespace aoc_2015_tests;
+
+public class Day15Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(-1, Day15.Part1());
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(-1, Day15.Part2());
+    }
+}
