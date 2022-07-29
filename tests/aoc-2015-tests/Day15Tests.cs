5cecf21237b5349941531bdb73547ca5542cd7a2
Joe Nace <jwnace@gmail.com>
2022-07-28 23:51:31 -0400

    updated tests for day 15

--- tests/aoc-2015-tests/Day15Tests.cs
+++ tests/aoc-2015-tests/Day15Tests.cs
@@ -7,12 +7,12 @@ public class Day15Tests
     [Fact]
     public void Part1()
     {
-        Assert.Equal(-1, Day15.Part1());
+        Assert.Equal(18965440, Day15.Part1());
     }
 
     [Fact]
     public void Part2()
     {
-        Assert.Equal(-1, Day15.Part2());
+        Assert.Equal(15862900, Day15.Part2());
     }
 }
