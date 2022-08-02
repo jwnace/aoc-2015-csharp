9bc85f2d35b05648a4607fc7b1ca814c39d6f118
Joe Nace <jwnace@gmail.com>
2022-08-01 23:03:28 -0400

    added solution for day 17

--- src/aoc-2015/Day17.cs
+++ src/aoc-2015/Day17.cs
@@ -1,26 +1,111 @@
-﻿using System.Security.AccessControl;
+﻿using System.Diagnostics;
 
 namespace aoc_2015;
 
 public static class Day17
 {
-    private record Container(int Size);
-    
+    private static readonly int TARGET_SIZE = 150;
+
+    private record Container(string Name, int Size);
+
     public static int Part1()
     {
-        var input = File.ReadAllLines("../../../../../input/day17.txt")
-            .Select(x => new Container(int.Parse(x)))
+        var stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+
+        var containers = File.ReadAllLines("../../../../../input/day17.txt")
+            .OrderBy(int.Parse)
+            .Select((x, i) => new Container($"{(char) (65 + i)}", int.Parse(x)))
             .ToList();
 
-        // var temp = new List<int> { 1, 2, 3 };
-        // var combinations = input.GetKCombs(input.Count - 1).ToList();
+        stopwatch.Stop();
+        Console.WriteLine($"Parsing the input took {stopwatch.Elapsed.TotalSeconds} seconds.");
+        stopwatch.Reset();
 
-        return 0;
+        stopwatch.Start();
+
+        var combinations = GetCombinations(containers);
+
+        stopwatch.Stop();
+        Console.WriteLine($"GetCombinations() took {stopwatch.Elapsed.TotalSeconds} seconds.");
+        stopwatch.Reset();
+
+        stopwatch.Start();
+
+        var query = combinations.Where(x => x.Sum(y => y.Size) == TARGET_SIZE).ToList();
+
+        stopwatch.Stop();
+        Console.WriteLine($"The query took {stopwatch.Elapsed.TotalSeconds} seconds.");
+        stopwatch.Reset();
+
+        return query.Count();
     }
 
     public static int Part2()
     {
-        var input = File.ReadAllLines("../../../../../input/day17.txt");
-        return 0;
+        var stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+
+        var containers = File.ReadAllLines("../../../../../input/day17.txt")
+            .OrderBy(int.Parse)
+            .Select((x, i) => new Container($"{(char) (65 + i)}", int.Parse(x)))
+            .ToList();
+
+        stopwatch.Stop();
+        Console.WriteLine($"Parsing the input took {stopwatch.Elapsed.TotalSeconds} seconds.");
+        stopwatch.Reset();
+
+        stopwatch.Start();
+
+        var combinations = GetCombinations(containers);
+
+        stopwatch.Stop();
+        Console.WriteLine($"GetCombinations() took {stopwatch.Elapsed.TotalSeconds} seconds.");
+        stopwatch.Reset();
+
+        stopwatch.Start();
+
+        var query = combinations
+            .Where(x => x.Sum(y => y.Size) == TARGET_SIZE)
+            .ToList();
+
+        var minCount = query.Min(y => y.Count);
+
+        var result = query.Where(x => x.Count == minCount).ToList();
+
+        stopwatch.Stop();
+        Console.WriteLine($"The query took {stopwatch.Elapsed.TotalSeconds} seconds.");
+        stopwatch.Reset();
+
+        return result.Count();
+    }
+
+    private static List<List<Container>> GetCombinations(List<Container> containers)
+    {
+        List<List<Container>> combinations = null;
+
+        for (int i = 1; i <= containers.Count(); i++)
+        {
+            var temp = GetCombinations(containers, i);
+            combinations = combinations == null ? temp : combinations.Concat(temp).ToList();
+        }
+
+        return combinations;
+    }
+
+    private static List<List<Container>> GetCombinations(List<Container> containers, int length)
+    {
+        if (length == 1)
+        {
+            return containers.Select(x => new List<Container> { x }).ToList();
+        }
+
+        return GetCombinations(containers, length - 1)
+            .SelectMany(
+                x => containers.Where(c => c.Name.CompareTo(x.Last().Name) > 0),
+                (t1, t2) => t1.Concat(new List<Container> { t2 }).ToList()
+            ).ToList();
     }
 }
