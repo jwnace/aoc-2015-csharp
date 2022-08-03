e915c75b6926a0318dd88100ae358f4baf393c28
Joe Nace <jwnace@gmail.com>
2022-08-03 00:07:05 -0400

    added brute force solution for day 20

---
+++ tests/aoc-2015-tests/Day20Tests.cs
@@ -0,0 +1,18 @@
+﻿using aoc_2015;
+
+namespace aoc_2015_tests;
+
+public class Day20Tests
+{
+    [Fact]
+    public void Part1()
+    {
+        Assert.Equal(831600, Day20.Part1());
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        Assert.Equal(884520, Day20.Part2());
+    }
+}
