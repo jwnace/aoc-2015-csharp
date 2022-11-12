49843500077e57e5977841ac66546aef195a74ac
Joe Nace <jwnace@gmail.com>
2022-11-11 22:33:27 -0500

    WIP day 24

---
+++ src/aoc-2015/Day24.cs
@@ -0,0 +1,18 @@
+namespace aoc_2015;
+
+public static class Day24
+{
+    public static int Part1()
+    {
+        var input = File.ReadAllLines("../../../../../input/day24.txt");
+
+        return -1;
+    }
+
+    public static int Part2()
+    {
+        var input = File.ReadAllLines("../../../../../input/day24.txt");
+        
+        return -2;
+    }
+}
