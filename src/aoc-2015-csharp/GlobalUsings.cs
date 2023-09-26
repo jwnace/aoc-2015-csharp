c28cda3e8d71d5d708caff75706469b39d12332b
Joe Nace <jwnace@gmail.com>
2023-09-26 14:58:49 -0400

    added global usings and uncommented solutions in program.cs

---
+++ src/aoc-2015-csharp/GlobalUsings.cs
@@ -0,0 +1,27 @@
+// Global using directives
+
+global using aoc_2015_csharp.Day01;
+global using aoc_2015_csharp.Day02;
+global using aoc_2015_csharp.Day03;
+global using aoc_2015_csharp.Day04;
+global using aoc_2015_csharp.Day05;
+global using aoc_2015_csharp.Day06;
+global using aoc_2015_csharp.Day07;
+global using aoc_2015_csharp.Day08;
+global using aoc_2015_csharp.Day09;
+global using aoc_2015_csharp.Day10;
+global using aoc_2015_csharp.Day11;
+global using aoc_2015_csharp.Day12;
+global using aoc_2015_csharp.Day13;
+global using aoc_2015_csharp.Day14;
+global using aoc_2015_csharp.Day15;
+global using aoc_2015_csharp.Day16;
+global using aoc_2015_csharp.Day17;
+global using aoc_2015_csharp.Day18;
+global using aoc_2015_csharp.Day19;
+global using aoc_2015_csharp.Day20;
+global using aoc_2015_csharp.Day21;
+global using aoc_2015_csharp.Day22;
+global using aoc_2015_csharp.Day23;
+global using aoc_2015_csharp.Day24;
+global using aoc_2015_csharp.Day25;
