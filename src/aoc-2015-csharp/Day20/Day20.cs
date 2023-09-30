c11fb44bae1fdc79134a33f544fa84493937cd39
Joe Nace <jwnace@gmail.com>
2023-09-29 22:02:26 -0400

    implemented a new algorithm for day 20

--- src/aoc-2015-csharp/Day20/Day20.cs
+++ src/aoc-2015-csharp/Day20/Day20.cs
@@ -2,73 +2,30 @@
 
 public static class Day20
 {
-    public static int Part1()
+    private static readonly int Input = int.Parse(File.ReadAllText("Day20/day20.txt"));
+
+    public static int Part1() => Solve(10);
+
+    public static int Part2() => Solve(11, 50);
+
+    private static int Solve(int presentsPerElf, int housesPerElf = 0)
     {
-        var input = int.Parse(File.ReadAllText("Day20/day20.txt"));
+        var max = Input / 20;
+        var houses = new int[max];
 
-        // HACK: there has to be a sane way to speed this up...
-        for (int i = 800000; i < int.MaxValue; i++)
+        for (var elf = 1; elf < max; elf++)
         {
-            var presents = GetPresents(i);
-
-            if (presents >= input)
+            for (var house = elf; house < max; house += elf)
             {
-                return i;
+                if (housesPerElf > 0 && elf * housesPerElf < house)
+                {
+                    break;
+                }
+
+                houses[house] += elf * presentsPerElf;
             }
         }
 
-        return 0;
-    }
-
-    public static int Part2()
-    {
-        var input = int.Parse(File.ReadAllText("Day20/day20.txt"));
-
-        // HACK: there has to be a sane way to speed this up...
-        for (int i = 800000; i < int.MaxValue; i++)
-        {
-            var presents = GetPresentsPart2(i);
-
-            if (presents >= input)
-            {
-                return i;
-            }
-        }
-
-        return 0;
-    }
-
-    private static IEnumerable<int> GetFactors(int number)
-    {
-        for (int i = 1; i <= number; i++)
-        {
-            if (number % i == 0)
-            {
-                yield return i;
-            }
-        }
-    }
-
-    private static int GetPresents(int i)
-    {
-        var factors = GetFactors(i);
-        return factors.Sum(x => x * 10);
-    }
-
-    private static IEnumerable<int> GetFactorsPart2(int number)
-    {
-        for (int i = 1; i <= number; i++)
-        {
-            if (number % i == 0 && number / i <= 50)
-            {
-                yield return i;
-            }
-        }
-    }
-
-    private static int GetPresentsPart2(int i)
-    {
-        var factors = GetFactorsPart2(i);
-        return factors.Sum(x => x * 11);
+        return houses.Select((presents, index) => (presents, index)).First(x => x.presents >= Input).index;
     }
 }
